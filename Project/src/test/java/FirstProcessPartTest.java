7b3e0c8c911a43ee35c5ea3c82de8bc96b113c19
puchnerdaniel <dpuchner@VMATVIE013.securityresearch.local>
2013-05-31 11:14:50 +0200

    further unit tests for diagram

--- Project/src/test/java/FirstProcessPartTest.java
+++ Project/src/test/java/FirstProcessPartTest.java
@@ -25,9 +25,12 @@ public class FirstProcessPartTest {
 	private static RuntimeService runtimeService;
 	private static ProcessEngine processEngine;
 	private static ProcessInstance processInstance;
-
+    private TaskService taskService;
+    private FormService formService;
+    private IdentityService identityService;
 	private String yourEMail = "yourEMail@yourHost.com";
 	private FinalDocument finalDocument;
+	private String pid;
 	
 	@BeforeClass
 	public static void init()
@@ -63,33 +66,132 @@ public class FirstProcessPartTest {
 	}
 	
 
-	
-	@Test
-	public void testFirstPartPositive(){
+	private void CreateNewProcess(){
 		processInstance = runtimeService.startProcessInstanceByKey("ProductCreationProcess");
 		assertNotNull(processInstance.getId());
-		IdentityService identityService = processEngine.getIdentityService();
-		TaskService taskService = processEngine.getTaskService(); 
-		String piId = processInstance.getProcessInstanceId();
-		// First task is evaluate requirements
-		FormService formService = processEngine.getFormService();
-
+		identityService = processEngine.getIdentityService();
+		taskService = processEngine.getTaskService(); 
+		pid = processInstance.getProcessInstanceId();
+		formService = processEngine.getFormService();
+	}
+	
+	private void evaluateRequirements(String meetsBusinessGoals, String isNewReceipe){
 		String evalRequirementsId = taskService.createTaskQuery().taskDefinitionKey("EvaluateRequirements").singleResult().getId();
 		//assertNotNull(evalRequirements);
-		FinalDocument finalDocument = new FinalDocument();
+		/*FinalDocument finalDocument = new FinalDocument();
 		finalDocument.MeetsBusinessGoals=true;
-		runtimeService.setVariable(piId, "finalDocument", finalDocument);
-		runtimeService.setVariable(piId, "test", true);
-		FinalDocument doc = (FinalDocument)runtimeService.getVariable(piId, "finalDocument");
+		runtimeService.setVariable(pid, "finalDocument", finalDocument);
+		runtimeService.setVariable(pid, "test", true);
+		FinalDocument doc = (FinalDocument)runtimeService.getVariable(pid, "finalDocument");*/
 		HashMap<String, String> map = new HashMap<String, String>();
-		map.put("MeetsBusinessGoals", "true");
+		map.put("MeetsBusinessGoals", meetsBusinessGoals);
+		map.put("IsNewReceipe", isNewReceipe);
 		formService.submitTaskFormData(evalRequirementsId, map);
-		
-		List<Task> tasks = taskService.createTaskQuery().list();
-		
-		// 
-	
-		
-		
 	}
+	
+	private void checkCreditWorthiness(String hasSolvency){
+		String checkCreditId = taskService.createTaskQuery().taskDefinitionKey("CheckCreditWorthiness").singleResult().getId();
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("HasSolvency", hasSolvency);
+		formService.submitTaskFormData(checkCreditId, map);
+	}
+	
+	private void assertNextTaskHasId(String taskId){
+		Task nextTasks = taskService.createTaskQuery().singleResult();
+		assertEquals(nextTasks.getTaskDefinitionKey(), taskId);
+	}
+	
+	private void checkLegalConstrains(String legalConstraintsOk){
+		String constraintsId = taskService.createTaskQuery().taskDefinitionKey("CheckLegalConstraints").singleResult().getId();
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("LegalConstraintsOk", legalConstraintsOk);
+		formService.submitTaskFormData(constraintsId, map);
+	}
+	
+	private void findSubstitute(String substituteFound){
+		String constraintsId = taskService.createTaskQuery().taskDefinitionKey("FindSubstitute").singleResult().getId();
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("SubstituteFound", substituteFound);
+		formService.submitTaskFormData(constraintsId, map);
+	}
+	
+	
+	
+	@Test
+	public void testEvalRequirementsPositive(){
+		CreateNewProcess();
+		
+		// First task is evaluate requirements
+		evaluateRequirements("true","true");
+		assertNextTaskHasId("CheckCreditWorthiness");
+	}
+	
+	@Test
+	public void testEvalRequirementsNegative(){
+		CreateNewProcess();
+		
+		// First task is evaluate requirements
+		evaluateRequirements("false","true");
+		// should be at end state
+		assertNull(taskService.createTaskQuery().singleResult());
+	}
+	
+	@Test
+	public void testCheckCreditWorthinessPositive(){
+		CreateNewProcess();
+		evaluateRequirements("true","true");
+		checkCreditWorthiness("true");
+		
+		assertNextTaskHasId("CheckLegalConstraints");
+	}
+	
+	@Test
+	public void testCheckCreditWorthinessNegative(){
+		CreateNewProcess();
+		evaluateRequirements("true","true");
+		checkCreditWorthiness("false");
+		// should be at end state
+		assertNull(taskService.createTaskQuery().singleResult());
+	}
+	
+	@Test
+	public void testCheckLegalConstraintsPositive(){
+		CreateNewProcess();
+		evaluateRequirements("true","true");
+		checkCreditWorthiness("true");
+		checkLegalConstrains("true");
+		assertNextTaskHasId("CompileProductionPlan");
+	}
+	
+	@Test
+	public void testCheckLegalConstraintsWithError(){
+		CreateNewProcess();
+		evaluateRequirements("true","true");
+		checkCreditWorthiness("true");
+		checkLegalConstrains("false");
+		assertNextTaskHasId("FindSubstitute");
+	}
+	
+	@Test
+	public void testFindSubstitutePositive(){
+		CreateNewProcess();
+		evaluateRequirements("true","true");
+		checkCreditWorthiness("true");
+		checkLegalConstrains("false");
+		findSubstitute("true");
+		assertNextTaskHasId("CheckLegalConstraints");
+	}
+	
+	@Test
+	public void testFindSubstituteNegative(){
+		CreateNewProcess();
+		evaluateRequirements("true","true");
+		checkCreditWorthiness("true");
+		checkLegalConstrains("false");
+		findSubstitute("false");
+		
+		// process will end here
+		assertNull(taskService.createTaskQuery().singleResult());
+	}
+	
 }
