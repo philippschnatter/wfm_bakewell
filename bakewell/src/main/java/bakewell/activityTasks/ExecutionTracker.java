1acbbdf2a61dcc3457891031cdfea8a259b2b07b
puchnerdaniel <dpuchner@VMATVIE013.securityresearch.local>
2013-06-01 14:21:08 +0200

    Moved tests to bakewell folder

---
+++ bakewell/src/main/java/bakewell/activityTasks/ExecutionTracker.java
@@ -0,0 +1,14 @@
+package bakewell.activityTasks;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.ExecutionListener;
+
+public class ExecutionTracker implements ExecutionListener{
+
+	@Override
+	public void notify(DelegateExecution execution) throws Exception {
+		System.out.println("Execution started: "+ execution.getEventName());
+		
+	}
+
+}
