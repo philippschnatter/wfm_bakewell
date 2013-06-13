62332032bf25821155f4292f013bb58962fb1ab9
sgoetzinger <stefan.goetzinger@student.tuwien.ac.at>
2013-06-13 10:36:43 +0200

    Login proof of concept

--- bakewell/src/test/java/bakewell/jsf/loginBean.java
+++ bakewell/src/test/java/bakewell/jsf/loginBean.java
@@ -8,6 +8,31 @@ package bakewell.jsf;
  *
  */
 public class loginBean {
+	private String name;
+	private String password;
+	
+	public String login() {
+		if(!name.isEmpty()) return "ok";
+		else return "denied";
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
 	public loginBean() {
 	}
 }
