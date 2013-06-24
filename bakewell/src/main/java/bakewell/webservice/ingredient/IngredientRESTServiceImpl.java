6fa2a1fd89d50e13535a85346dbd5b9eeea624f0
Philipp Schnatter <schnaxl@gmail.com>
2013-06-24 17:47:57 +0200

    updated webservices to reflect DAO changes

--- bakewell/src/main/java/bakewell/webservice/ingredient/IngredientRESTServiceImpl.java
+++ bakewell/src/main/java/bakewell/webservice/ingredient/IngredientRESTServiceImpl.java
@@ -1,6 +1,5 @@
 package bakewell.webservice.ingredient;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.ws.rs.QueryParam;
@@ -9,15 +8,20 @@ import bakewell.beans.Ingredient;
 
 
 import bakewell.beans.Recipe;
-import bakewell.db.IngredientDAO;
+import bakewell.db.ProductDAO;
+import bakewell.db.RecipeDAO;
 
 public class IngredientRESTServiceImpl implements IngredientRESTService {
 	
-	private IngredientDAO ingdao;
+	private ProductDAO prodao;
+	private RecipeDAO recdao;
+	private final String USER = "sa";
+	private final String PW = "";
 	
 	public IngredientRESTServiceImpl() {
 		
-		this.ingdao = new IngredientDAO("sa", "");
+		this.prodao = new ProductDAO (USER, PW);
+		this.recdao = new RecipeDAO(USER, PW);
 	}
 
 	@Override
@@ -28,17 +32,17 @@ public class IngredientRESTServiceImpl implements IngredientRESTService {
 		// DAO: ich habe eine Product ID und brauche alle Ingredients + Mengen (in Gramm) jeweils dazu
 		// TODO DAO needed!!!!!
 
-		Map<Ingredient, Double> ingredientmap = new HashMap<Ingredient, Double>();
-//		Map<Ingredient, Double> ingredientmap = ingdao.getIngredientsOfProduct(productid);
+//		Map<Ingredient, Double> ingredientmap = new HashMap<Ingredient, Double>();
+		Map<Ingredient, Double> ingredientmap = prodao.selectIngredientsOfProduct(productid);
 		
 		// statt der DAO temporaer eigene objekte
-		Ingredient ing_mehl = new Ingredient("Mehl", 897.0, 43.0, 10.0, 20.0, 7.0, 2.0, 23.0);
-		Ingredient ing_ei = new Ingredient("Ei", 223.0, 10.0, 30.0, 9.0, 21.0, 3.0, 20.0);
-		Ingredient ing_schokolade = new Ingredient("Schokolade", 893.0, 15.0, 50.0, 5.0, 5.0, 2.0, 27.0);
-		
-		ingredientmap.put(ing_mehl, 250.0);
-		ingredientmap.put(ing_ei, 120.0);
-		ingredientmap.put(ing_schokolade, 80.0);
+//		Ingredient ing_mehl = new Ingredient("Mehl", 897.0, 43.0, 10.0, 20.0, 7.0, 2.0, 23.0);
+//		Ingredient ing_ei = new Ingredient("Ei", 223.0, 10.0, 30.0, 9.0, 21.0, 3.0, 20.0);
+//		Ingredient ing_schokolade = new Ingredient("Schokolade", 893.0, 15.0, 50.0, 5.0, 5.0, 2.0, 27.0);
+//		
+//		ingredientmap.put(ing_mehl, 250.0);
+//		ingredientmap.put(ing_ei, 120.0);
+//		ingredientmap.put(ing_schokolade, 80.0);
 		// end temp stuff
 		
 		// Values for all ingredients
@@ -85,10 +89,12 @@ public class IngredientRESTServiceImpl implements IngredientRESTService {
 		allgda_fiber = allgda_fiber / GdaTable.RECGDA_FIBER.getGramPerDay() * 100;
 		allgda_sodium = allgda_sodium / GdaTable.RECGDA_SODIUM.getGramPerDay() * 100;
 		
-		Recipe newrec = new Recipe("some name", "some description", allgda_energy, allgda_protein, allgda_carbo, allgda_fat, allgda_fiber, allgda_sodium, 0.0);
-		Recipe oldrec = ingdao.getRecipeByProductId(productid);
+		Recipe oldrec = prodao.selectRecipeByProductId(productid);
+		Recipe newrec = new Recipe(oldrec.getName(), oldrec.getDescription(), allgda_energy, 
+				allgda_protein, allgda_carbo, allgda_fat, allgda_fiber, allgda_sodium, 0.0);
 		
-		ingdao.updateRecipe(oldrec.getId(), newrec);
+		
+		recdao.UpdateRecipe(newrec, oldrec);
 		
 		return newrec;
 	}
