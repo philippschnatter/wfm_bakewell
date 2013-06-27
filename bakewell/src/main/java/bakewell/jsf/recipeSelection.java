b68bcb3d941b82d26ff1152381fa1f9599129c92
sgoetzinger <stefan.goetzinger@student.tuwien.ac.at>
2013-06-27 05:42:25 +0200

    forms, beans

--- bakewell/src/main/java/bakewell/jsf/recipeSelection.java
+++ bakewell/src/main/java/bakewell/jsf/recipeSelection.java
@@ -1,13 +1,20 @@
 package bakewell.jsf;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
+import org.activiti.engine.FormService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RuntimeService;
+
 import bakewell.beans.Product;
 import bakewell.beans.Recipe;
+import bakewell.jsf.jsfService;
 
 /**
  * 
@@ -15,9 +22,16 @@ import bakewell.beans.Recipe;
  *
  */
 public class recipeSelection {
-
-	String taskId="";
-	jsfService jsfService=null;
+	HttpServletRequest request = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+	String taskId = request.getParameter("taskId");
+	String pid = request.getParameter("pid");
+	
+	jsfService jsfService = new jsfService();
+	
+	private String transport_Contractor = "";
+	private Double transport_cost = null;
+	private Integer productId = null;
+	
 	ArrayList<Recipe> recipeList=null;
 	Integer selectedRecipeId=null;
 
@@ -52,8 +66,15 @@ public class recipeSelection {
 	public void setSelectedRecipe(Integer selectedRecipeId) {
 		this.selectedRecipeId = selectedRecipeId;
 	}
-
+	
 	public String proceed(){
+		ActivitiFactory engine = ActivitiFactory.getInstance();
+		ProcessEngine processEngine = engine.getProcessEngine();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		FormService formService = processEngine.getFormService();
+//		RuntimeService runtimeService = processEngine.getRuntimeService();
+		Map<String, String> map = new HashMap<String, String>();
+		
 		if(selectedRecipeId!=null){
 			String processId = null;
 			Integer productId = (Integer)runtimeService.getVariable(processId, "productId");
@@ -62,7 +83,19 @@ public class recipeSelection {
 			temp.setRecipe_id(selectedRecipeId);
 			
 			jsfService.updateProduct(temp);
+			
+			map.put("IsNewRecipe", "false");
 		}
+		else {
+			map.put("IsNewRecipe", "true");
+		}
+
+		
+//		map.put("IsNewRecipe", "true");
+//		runtimeService.setVariable(engine.getInstance().toString(), "MeetsBusinessGoals", "true");
+		formService.submitTaskFormData(this.taskId, map);
+		
+
 		//TODO Activiti black magic stuff for proceeding to next form
 		return "proceed";
 	}
